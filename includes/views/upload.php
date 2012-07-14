587ad684903fbb16b5c9ec3ec1b1784821ee74f0
Pascal Hofmann <mail@pascalhofmann.de>
2012-07-14 21:51:55 +0200

    Fixed "notices".

--- includes/views/upload.php
+++ includes/views/upload.php
@@ -1,10 +1,14 @@
 <?php render('_header', array('title' => $title))?>
+<?php if (!empty($alerts['error'])) : ?>
 	<?php foreach ($alerts['error'] as $message) : ?>
 		<div class="alert alert-error"><?php echo $message?></div>
 	<?php endforeach ?> 
+<?php endif ?>
+<?php if (!empty($alerts['success'])) : ?>
 	<?php foreach ($alerts['success'] as $message) : ?>
 		<div class="alert alert-success"><?php echo $message?></div>
 	<?php endforeach ?> 
+<?php endif ?>
 <div class="row">
 	<div class="span12">
 		<?php render('_upload'); ?>
