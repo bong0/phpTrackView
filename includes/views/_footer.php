f7fed3ec31ac6a402ff6d920f8f6fa2b9c7c41dc
Pascal Hofmann <mail@pascalhofmann.de>
2012-07-15 15:24:24 +0200

    Added fixed footer

--- includes/views/_footer.php
+++ includes/views/_footer.php
@@ -1,4 +1,10 @@
-		<?php echo get_execution_time() ?>
+		</div>
+		<div class="navbar navbar-fixed-bottom clearfix">
+			<div class="navbar-inner">
+				<div class="container">
+					<p class="navbar-text"><?php printf('%0.5fs', get_execution_time()) ?></p>
+				</div>
+			</div>
 		</div>
 	</body>
 </html>
